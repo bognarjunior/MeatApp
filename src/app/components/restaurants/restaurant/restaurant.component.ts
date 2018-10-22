5bd0bd88e2c957acd199b26a87a2c7ede73cb5ea
Bognar Junior <bognar_junior@yahoo.com.br>
2018-10-21 22:48:59 -0300

    Criando modelo
    Criando o model do restaurante

--- src/app/components/restaurants/restaurant/restaurant.component.ts
+++ src/app/components/restaurants/restaurant/restaurant.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'mt-restaurant',
   templateUrl: './restaurant.component.html'
 })
 export class RestaurantComponent implements OnInit {
+
+  @Input()
+  restaurant: any;
 
   constructor() { }
 
