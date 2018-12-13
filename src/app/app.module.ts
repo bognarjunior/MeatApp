3dc72a1ca580f28ecfe92fe57a299941869e4a30
Bognar Junior <bognar_junior@yahoo.com.br>
2018-12-12 23:30:50 -0200

    Alterando localização
    Alterando localização para pt-BR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
@@ -42,7 +42,12 @@ import {
     RouterModule.forRoot(ROUTES)
   ],
   providers: [
-    RestaurantService, ShoppingCartService
+    RestaurantService,
+    ShoppingCartService,
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt-BR'
+    }
   ],
   bootstrap: [AppComponent]
 })
