400103846651cbeb4ef346f46aaf71c98a103a67
Bognar Junior <bognar_junior@yahoo.com.br>
2018-12-12 22:42:58 -0200

    Update shopping-cart.component.ts
    Implementando as funcionalidades no componente shopping-cart

--- src/app/components/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/components/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ShoppingCartService } from './shopping-cart.service';
+import { CartItem } from './cart-item.model';
 
 @Component({
   selector: 'mt-shopping-cart',
@@ -6,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingCartComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private shoppingCardService: ShoppingCartService
+  ) { }
 
   ngOnInit() {
+  }
+
+  items(): CartItem[] {
+    return this.shoppingCardService.items;
+  }
+
+  total(): number {
+    return this.shoppingCardService.total();
   }
 
 }
