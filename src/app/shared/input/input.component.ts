203511d3bfff19cceea9e00b95c51d3ca3a8df1c
Bognar Junior <bognar_junior@yahoo.com.br>
2018-12-25 18:25:21 -0200

    Refatorando componente order
    Refatorando o componente order

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,14 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ContentChild, AfterContentInit } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
-  selector: 'mt-input',
+  selector: 'mt-input-container',
   templateUrl: './input.component.html'
 })
-export class InputComponent implements OnInit {
+export class InputComponent implements OnInit, AfterContentInit {
+
+  inputValue: any;
+
+  @Input()
+  label: string;
+
+  @Input()
+  errorMessage: string;
+
+  @ContentChild(NgModel)
+  model: NgModel;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterContentInit() {
+    this.inputValue = this.model;
+    if (this.inputValue === undefined) {
+      throw new Error('Este componente precisa ser utilizado com uma diretiva ngModel');
+    }
+  }
+
+  hasSuccess(): boolean{
+    return this.inputValue.valid && (this.inputValue.dirty || this.inputValue.touched);
+  }
+
+  hasError(): boolean{
+    return !this.inputValue.valid && (this.inputValue.dirty || this.inputValue.touched);
   }
 
 }
