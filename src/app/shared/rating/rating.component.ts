35a66a6b4c34b39003f9eb11786852cd3ba6a20b
Bognar Junior <bognar_junior@yahoo.com.br>
2018-12-28 21:35:45 -0200

    Update component rating
    Atualização do componente de avaliação

--- src/app/shared/rating/rating.component.ts
+++ src/app/shared/rating/rating.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'mt-rating',
@@ -6,9 +6,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RatingComponent implements OnInit {
 
+  @Output()
+  rated = new EventEmitter<number>();
+
+  rates = [1, 2, 3, 4, 5];
+
+  rate = 0;
+
+  previousRate: number;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  setRate(r: number) {
+    this.rate = r;
+    this.previousRate = undefined;
+    this.rated.emit(this.rate);
+  }
+
+  setTemporaryRate(r: number) {
+    if (this.previousRate === undefined) {
+      this.previousRate = this.rate;
+    }
+    this.rate = r;
+  }
+
+  clearTemporaryRate() {
+    if (this.previousRate !== undefined) {
+      this.rate = this.previousRate;
+      this.previousRate = undefined;
+    }
   }
 
 }
