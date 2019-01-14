2c6a8efb84a1907cfb0476abffa3615cae9a84b1
Bognar Junior <bognar_junior@yahoo.com.br>
2019-01-14 21:14:56 -0200

    Create shared.module.ts
    Criando módulo compartilhado

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { InputComponent } from './input';
+import { RadioComponent } from './radio';
+import { RatingComponent } from './rating';
+
+@NgModule({
+    declarations: [
+        InputComponent,
+        RadioComponent,
+        RatingComponent
+    ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule
+    ],
+    exports: [
+        InputComponent,
+        RadioComponent,
+        RatingComponent,
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule
+    ]
+})
+export class SharedModule {}
