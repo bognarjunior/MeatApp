7f269cc626a2e637e390d55625f0c0d8f58dcde0
Bognar Junior <bognar_junior@yahoo.com.br>
2019-01-15 21:24:23 -0200

    Add PreloadAllModules in app.module
    Adicionando o Preload na aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, PreloadAllModules } from '@angular/router';
 
 import { ROUTES } from 'app/app.routes';
 
@@ -39,7 +39,10 @@ import { SharedModule } from 'app/shared';
     BrowserModule,
     HttpModule,
     SharedModule.forRoot(),
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(ROUTES, 
+      {
+        preloadingStrategy: PreloadAllModules
+      })
   ],
   providers: [
     {
