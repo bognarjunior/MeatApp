7390a0771dd44c6d4be877f411c893e838bc3baf
Bognar Junior <bognar_junior@yahoo.com.br>
2019-01-19 18:38:52 -0200

    Update shopping-cart.service with Notification service
    Atualizando o shopping cart service para receber emitir uma mensagem quando adicionar ou remover item do carrinho

--- src/app/components/restaurant-detail/shopping-cart/shopping-cart.service.ts
+++ src/app/components/restaurant-detail/shopping-cart/shopping-cart.service.ts
@@ -1,8 +1,15 @@
+import { Injectable } from '@angular/core';
 import { CartItem } from './cart-item.model';
 import { MenuItem } from '../menu-item';
+import { NotificationService } from 'app/shared/messages';
 
+@Injectable()
 export class ShoppingCartService {
     items: CartItem[] = [];
+
+    constructor(
+        private notificationService: NotificationService
+    ) {}
 
     addItem(item: MenuItem) {
         let foundItem = this.items.find( mItem => mItem.menuItem.id === item.id);
@@ -12,6 +19,8 @@ export class ShoppingCartService {
         } else {
             this.items.push(new CartItem(item));
         }
+
+        this.notificationService.notify(`Você adicionou o item ${item.name}`);
     }
 
     increaseQty(item: CartItem) {
@@ -27,6 +36,7 @@ export class ShoppingCartService {
 
     removeItem(item: CartItem) {
         this.items.splice(this.items.indexOf(item), 1);
+        this.notificationService.notify(`Você adicionou o item ${item.menuItem.name}`);
     }
 
     clear(): void {
