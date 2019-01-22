1b808d3b31f25c057c61b6c9539c899fcb310c98
Bognar Junior <bognar_junior@yahoo.com.br>
2019-01-21 22:02:24 -0200

    Create animations search bar
    Criando a animação da barra de busca

--- src/app/components/restaurants/restaurants.component.ts
+++ src/app/components/restaurants/restaurants.component.ts
@@ -1,13 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
 import { Restaurant, RestaurantService } from './restaurant';
 
 @Component({
   selector: 'mt-restaurants',
   templateUrl: './restaurants.component.html',
+  animations: [
+    trigger('toggleSearch', [
+      state('hidden', style({
+        opacity: 0,
+        'max-height': '0px',
+      })),
+      state('visible', style({
+        opacity: 1,
+        'max-height': '70px',
+        'margin-top': '20px'
+      })),
+      transition('* => *', animate('250ms 0s ease-in-out'))
+    ])
+  ]
 })
 export class RestaurantsComponent implements OnInit {
 
   restaurants: Restaurant[];
+  searchBarState = 'hidden';
 
   constructor(private restaurantService: RestaurantService) { }
 
@@ -16,4 +33,7 @@ export class RestaurantsComponent implements OnInit {
       .subscribe(restaurants => this.restaurants = restaurants);
   }
 
+  toggleSearch(): void {
+    this.searchBarState = this.searchBarState === 'hidden' ? 'visible' : 'hidden';
+  }
 }
