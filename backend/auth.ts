2e59dbbd23dd0fc337808d2234c78bceea10378f
Bognar Junior <bognar_junior@yahoo.com.br>
2019-01-28 21:32:15 -0200

    Adding JWT Token to Login Path
    Adicionando Token JWT a Rota de Login

--- backend/auth.ts
+++ backend/auth.ts
@@ -1,13 +1,20 @@
 import { Request, Response } from 'express';
+import * as jwt from 'jsonwebtoken';
+
 import { User, users } from './users';
 
 export const handleAuthentication = (req: Request, res: Response) => {
     const user: User = req.body;
     if (isValid(user)) {
         const dbUser: User = users[user.email];
+        const token = jwt.sign({
+            sub: dbUser.email,
+            iss: 'meat-api'
+        }, 'meat-api-password');
         res.json({
             name: dbUser.name,
-            email: dbUser.email
+            email: dbUser.email,
+            accessToken: token
         })
     } else {
         res.status(403).json({
--- backend/dist/auth.js
+++ backend/dist/auth.js
@@ -1,13 +1,19 @@
 "use strict";
 exports.__esModule = true;
+var jwt = require("jsonwebtoken");
 var users_1 = require("./users");
 exports.handleAuthentication = function (req, res) {
     var user = req.body;
     if (isValid(user)) {
         var dbUser = users_1.users[user.email];
+        var token = jwt.sign({
+            sub: dbUser.email,
+            iss: 'meat-api'
+        }, 'meat-api-password');
         res.json({
             name: dbUser.name,
-            email: dbUser.email
+            email: dbUser.email,
+            accessToken: token
         });
     }
     else {
