214d61624d428cd8d2abedff0d38f3e939f0e25d
Bognar Junior <bognar_junior@yahoo.com.br>
2019-02-06 19:36:13 -0200

    preserveWhitespaces
    Ajustando o preserveWhitespaces

--- src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -8,6 +8,7 @@ import { MenuItem } from '../menu-item';
 @Component({
   selector: 'mt-shopping-cart',
   templateUrl: './shopping-cart.component.html',
+  preserveWhitespaces: true,
   animations: [
     trigger('row', [
       state('ready', style({ opacity: 1 })),
